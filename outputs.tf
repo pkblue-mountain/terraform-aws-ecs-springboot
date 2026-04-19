62309b5ae98ac8c8b093463d7a4ab7f00be95e97
pk <pk.jio.660@gmail.com>
2026-04-19 23:48:16 +0530

    fix: comment out repository_url output in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "repository_url" {
-  value = aws_ecr_repository.springboot_app.repository_url
-}
+# output "repository_url" {
+#   value = aws_ecr_repository.springboot_app.repository_url
+# }
 
 output "alb_dns" {
   value = aws_lb.app.dns_name
