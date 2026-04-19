23405d1ad210d579c5815fc54601a71a2a45c464
pk <pk.jio.660@gmail.com>
2026-04-19 22:52:47 +0530

    fix: correct syntax for CloudWatch log group reference in ECS task definition

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_ecs_task_definition" "app" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "aws_cloudwatch_log_group.ecs_logs.name"
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
           awslogs-region        = "ap-south-2"
           awslogs-stream-prefix = "ecs"
         }
